82a7c10e90af59171df0699003b0e3dcd68c334b
kevinhenneigh <kevinhenneigh@hotmail.com>
2020-07-09 12:21:00 -0700

    Created Student object, Add form, and Student form

---
+++ ModelsAndModelBinding/Controllers/StudentController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ModelsAndModelBinding.Models;
+
+namespace ModelsAndModelBinding.Controllers
+{
+    public class StudentController : Controller
+    {
+        [HttpGet] // When user navigates to url
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost] // Call this Add method after user posts form data
+        public IActionResult Add(IFormCollection form)
+        {
+            Student stu = new Student();
+            stu.FullName = form["full_name"];
+            stu.DateOfBirth = Convert.ToDateTime(form["dob"]);
+            stu.PhoneNumber = form["home_phone"];
+            stu.EmailAddress = form["email"];
+
+            return View();
+        }
+    }
+}
